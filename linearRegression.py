eae262a96002c82fa1e76191b2e15f5c7db75fd3
arenuia <arenui.anderson@gmail.com>
2018-10-12 00:45:42 -0600

    i cleaned up the names and im gonna close this

--- linearRegression.py
+++ linearRegression.py
@@ -64,6 +64,8 @@ plt.show()
 plt.plot(no_int_model.predict(hwdataTrain), no_int_model.resid, '.')
 # The prediction plot seems to show constant variance, meaning there is no heteroscedasticity.
 
+# This is the last thing that I really looked at. The rest is mainly changing names to be descriptive and taking away redundant stuff.
+
 #(1)TODO:have a stat guy look at
 # The R-squared variance is less for our linear model with the intercept meaning that our data fits the first linear model better. The error variance is within a near range of y for each x as x and y increases, this is more true for our first model than our second so we can say this is a better fit. The errors are independent of each other we see this by the absence of any major defects in our graph along with no abnormal clumps of data in either graph.
 #If we were to use R^2 as our function then our model then our residual would be at least even greater, so it wont fit the data better.
@@ -72,20 +74,17 @@ plt.plot(no_int_model.predict(hwdataTrain), no_int_model.resid, '.')
 ########## Part2 ##########
 print("########## Part 2 ##########")
 
-hwdata = pd.read_csv(os.path.join(cwd, 'height_weight2.csv'))
-hwdata.columns = ['height', 'weight']
+hwdata2 = pd.read_csv(os.path.join(cwd, 'height_weight2.csv'))
+hwdata2.columns = ['height', 'weight']
 
-hwdataTrain, hwdataTest = train_test_split(hwdata, test_size=.3, random_state=123)
-hwdataTrain.shape
-hwdataTest.shape
+hwdata2Train, hwdata2Test = train_test_split(hwdata2, test_size=.3, random_state=123)
 
-model = smf.ols(formula='weight ~ 0 + height', data=hwdataTrain).fit()
-print(model.summary())
+no_int_model2 = smf.ols(formula='weight ~ 0 + height', data=hwdata2Train).fit()
+no_int_model2.summary()
 
-residual = model.resid
-plt.hist(residual, 50)
+plt.hist(no_int_model2.resid, 50)
 plt.show()
-
+# Residuals seem to be normal, with mean 0.
 
 #(2)TODO:
 #
@@ -93,18 +92,15 @@ plt.show()
 ########## Part 3 ##########
 print("########## Part 3 ##########")
 
-hwdata = pd.read_csv(os.path.join(cwd, 'car.csv'))
-hwdata.columns = ['age', 'make', 'type', 'miles', 'price']
+cardata = pd.read_csv(os.path.join(cwd, 'car.csv'))
+cardata.columns = ['age', 'make', 'type', 'miles', 'price']
 
-hwdataTrain, hwdataTest = train_test_split(hwdata, test_size=.3, random_state=123)
-hwdataTrain.shape
-hwdataTest.shape
+cardataTrain, cardataTest = train_test_split(cardata, test_size=.3, random_state=123)
 
-model = smf.ols(formula='price ~ age + type + miles', data=hwdataTrain).fit()
-print(model.summary())
+car_model = smf.ols(formula='price ~ age + type + miles', data=cardataTrain).fit()
+car_model.summary()
 
-residual = model.resid
-plt.hist(residual, 50)
+plt.hist(car_model.resid, 50)
 plt.show()
 
 
