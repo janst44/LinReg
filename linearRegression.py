67fb8a9de7afc295ace6911b777f29c32b7f26ba
janst44 <joshua.c4mpbell@gmail.com>
2018-10-12 09:53:17 -0600

    finished last 2 tests in part 3

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,7 @@
+ 
+# coding: utf-8
 
-### Logistic Regression
+# # Logistic Regression
 
 import pandas as pd
 import numpy as np
@@ -14,95 +16,110 @@ cwd = os.getcwd()
 
 
 
-# THE THREE CONDITIONS:
-# 1. The error variable is normally distributed.
-# 2. The error variance is constant for all values of x.
-# 3. The errors are independent of each other.
-
-########## Part1 ##########
-print("########## Part 1 ##########")
+#THE THREE CONDITIONS:
+#1.The error variable is normally distributed.
+#2.The error variance is constant for all values of x.
+#3.The errors are independent of each other.
+##########################Part1###################
+#print(cwd)
+print("\n\n#################PART1###############")
 hwdata = pd.read_csv(os.path.join(cwd, 'height_weight1.csv'))
 hwdata.columns = ['height', 'weight']
-hwdata.head()
+#print(hwdata.head())
 
-### VARIABLE DESCRIPTIONS
-
+# ##### VARIABLE DESCRIPTIONS
+#
 # Height - in inches
 # Weight - in lbs
 
-hwdata.describe()
+#print(hwdata.dtypes)
 
-### Split data into training, test
 
-hwdataTrain, hwdataTest = train_test_split(hwdata, test_size=.3, random_state=5203)
-plt.plot(hwdataTrain[['height']], hwdataTrain[['weight']], ".")
+#print(hwdata.describe())
 
-### Creating Intercept Model
-int_model = smf.ols(formula='weight ~ 1 + height', data=hwdataTrain).fit()
-int_model.summary()
+### split data into training, test
+hwdataTrain, hwdataTest = train_test_split(hwdata, test_size=.3, random_state=123)
 
-### Test our residuals
-# Test for normalality of residuals
-plt.hist(int_model.resid, 50)
-plt.show()
-# Residuals seem to be normal, with mean 0.
-
-# Test for heteroscedasticity
-plt.plot(int_model.predict(hwdataTrain), int_model.resid, '.')
-# The prediction plot seems to show constant variance, with no heteroscedasticity.
-
-### Creating No Intercept Model
-no_int_model = smf.ols(formula='weight ~ 0 + height', data=hwdataTrain).fit()
-no_int_model.summary()
+#plt.plot(hwdataTrain[['height']], hwdataTrain[['weight']], '.')
+#plt.show()
+model = smf.ols(formula='weight ~ 1 + height', data=hwdataTrain).fit()
+print("Model with intercept summary")
+print(model.summary())
 
 ### Test our residuals
-# Test for normalality of residuals
-plt.hist(no_int_model.resid, 50)
-plt.show()
+print("Test of residuals:")
+residual = model.resid
+print(model.summary())
 
+# Test for normal residuals
+#plt.hist(residual, 50)
+#plt.show()
 # Test for heteroscedasticity
-plt.plot(no_int_model.predict(hwdataTrain), no_int_model.resid, '.')
-# The prediction plot seems to show constant variance, meaning there is no heteroscedasticity.
+#plt.plot(model.predict(hwdataTrain), residual, '.')
+#plt.show()
 
-# This is the last thing that I really looked at. The rest is mainly changing names to be descriptive and taking away redundant stuff.
+without_intercept = smf.ols(formula='weight ~ 0 + height', data=hwdataTrain).fit()
+print("Model without intercept summary")
+print(without_intercept.summary())
 
-#(1)TODO:have a stat guy look at
-# The R-squared variance is less for our linear model with the intercept meaning that our data fits the first linear model better. The error variance is within a near range of y for each x as x and y increases, this is more true for our first model than our second so we can say this is a better fit. The errors are independent of each other we see this by the absence of any major defects in our graph along with no abnormal clumps of data in either graph.
-#If we were to use R^2 as our function then our model then our residual would be at least even greater, so it wont fit the data better.
+### Test our residuals
+residual = without_intercept.resid
+# Test for normal residuals
+#plt.hist(residual, 50)
+#plt.show()
+# Test for heteroscedasticity
+#plt.plot(without_intercept.predict(hwdataTrain), residual, '.')
+#plt.show()
 
 
-########## Part2 ##########
-print("########## Part 2 ##########")
+#(1)
+#The error variance is normally distributed we see in our model without the intercept, this is we can see from the bell-curve histogram. The error variance is constant for all values as show from the plot of the points. No abnormal clumps of data in the graph w/without the intercept so the data is independent.
+#If we were to use R^2 then that would help us acheive a higher R^2 to fit the data even better.
 
-hwdata2 = pd.read_csv(os.path.join(cwd, 'height_weight2.csv'))
-hwdata2.columns = ['height', 'weight']
+###################Part2###########################
+print("\n\n############PART2###########")
 
-hwdata2Train, hwdata2Test = train_test_split(hwdata2, test_size=.3, random_state=123)
+hwdata = pd.read_csv(os.path.join(cwd, 'height_weight2.csv'))
+hwdata.columns = ['height', 'weight']
 
-no_int_model2 = smf.ols(formula='weight ~ 0 + height', data=hwdata2Train).fit()
-no_int_model2.summary()
+hwdataTrain, hwdataTest = train_test_split(hwdata, test_size=.3, random_state=123)
 
-plt.hist(no_int_model2.resid, 50)
-plt.show()
-# Residuals seem to be normal, with mean 0.
+model = smf.ols(formula='weight ~ 0 + height', data=hwdataTrain).fit()
+print("Model summary without intercept")
+print(model.summary())
 
-#(2)TODO:
-#
+residual = model.resid
+#plt.hist(residual, 50)
+#plt.show()
 
-########## Part 3 ##########
-print("########## Part 3 ##########")
+
+#(2)
+#THis model does meet all 3 residual assumptions. If one were not met however, that would mean that our predictions would have a larger margin for error. This is also true of point prediction, this is because the prediction interval will have increased and so point prediction is no longer accurate.
+
+###################Part3###########################
+print("\n\n############PART3###########")
 
 cardata = pd.read_csv(os.path.join(cwd, 'car.csv'))
-cardata.columns = ['age', 'make', 'type', 'miles', 'price']
-
 cardataTrain, cardataTest = train_test_split(cardata, test_size=.3, random_state=123)
 
-car_model = smf.ols(formula='price ~ age + type + miles', data=cardataTrain).fit()
-car_model.summary()
+print("\n\n#########  Model 1:   ###########")
+model = smf.ols(formula='Price ~ 1 + Age +  Miles + C(Make) + C(Type)', data=cardataTrain).fit()
+print(model.summary())
 
-plt.hist(car_model.resid, 50)
-plt.show()
+print("\n\n##########    Model 2:   ##########")
+model = smf.ols(formula='np.log(Price) ~ 1 + np.log(Age) +  np.log(Miles) + C(Make) + C(Type)', data=cardataTrain).fit()
+print(model.summary())
+
+#Best fit model
+print("\n\n###########    Model 3 and best fit:   ############")
+model = smf.ols(formula='Price ~ 1 + np.log(Age) +  np.log(Miles) + C(Make) + C(Type)', data=cardataTrain).fit()
+print(model.summary())
+
+#residual = model.resid
+#plt.hist(residual, 50)
+#plt.show()
 
 
-#(3)TODO:
-#
+#(3) Our prediction of a 7 year old BMW with 67,000 miles: 25661.29, below is math using coefficients and our intercept using our model of best-fit:
+#   5.78*10^4  +  -1776.7873 * 7 + -.1553 * 67000 + -9296.0945 * 1 = 25661.29
+
